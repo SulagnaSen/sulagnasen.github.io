2d3ed102c3d9a4e3b064bd500e5ea301c5c63a25
SulagnaSen <sulagnasen2404@gmail.com>
2024-03-11 16:15:33 -0400

    Updated references to image favicon

--- projects/script.js
+++ projects/script.js
@@ -21,7 +21,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Projects | Portfolio Sulagna Sen";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            $("#favicon").attr("href", "/assets/images/favicon.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
